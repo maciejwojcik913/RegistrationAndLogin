b43dfb1f26b696682cf9b00302b755abd3d90069
Maciej Wójcik <maciejwojcik913@gmail.com>
2023-03-11 01:28:46 +0100

    Initial commit

---
+++ src/test/java/io/github/maciejwojcik913/RegistrationAndLogin/RegistrationAndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maciejwojcik913.RegistrationAndLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAndLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
