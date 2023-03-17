f3241685a8662221db01737d511f04616497e286
Maciej Wójcik <maciejwojcik913@gmail.com>
2023-03-17 21:30:50 +0100

    Creating User and UserRole

---
+++ src/main/java/io/github/maciejwojcik913/RegistrationAndLogin/user/User.java
@@ -0,0 +1,123 @@
+package io.github.maciejwojcik913.RegistrationAndLogin.user;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+    public static final long serialVersionUID = 13L;
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 30, nullable = false, unique = true)
+    private String login;
+
+    @Column(length = 60, nullable = false)
+    private String password;
+
+    @Column(length = 60, name = "first_name")
+    private String firstname;
+
+    @Column(length = 60, name = "last_name")
+    private String lastname;
+
+    private boolean enabled = true;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<UserRole> roles = new HashSet<>();
+
+    /**
+     * used by Hibernate
+     */
+    public User() {
+    }
+
+    public User(String email, String login, String password) {
+        this.email = email;
+        this.login = login;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<UserRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<UserRole> roles) {
+        this.roles = roles;
+    }
+
+    public void addRole(UserRole role) {
+        this.roles.add(role);
+    }
+
+    public void removeRole(UserRole role) {
+        this.roles.remove(role);
+    }
+}
---
+++ src/main/java/io/github/maciejwojcik913/RegistrationAndLogin/user/UserRole.java
@@ -0,0 +1,42 @@
+package io.github.maciejwojcik913.RegistrationAndLogin.user;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "roles")
+public class UserRole implements Serializable {
+    public static final long serialVersionUID = 13L;
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 30)
+    private String name;
+
+    /**
+     * used by Hibernate
+     */
+    public UserRole() {
+    }
+
+    public UserRole(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
